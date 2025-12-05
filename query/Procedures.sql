078f03d956f1ab9a7b1724f286548270dc1585c0
Kauan Selyson <122841788+KSelyson@users.noreply.github.com>
2025-12-04 21:48:23 -0300

    Reestruturando a procedure
    Ajustando os nomes e atributos da procedure

--- query/Procedures.sql
+++ query/Procedures.sql
@@ -6,35 +6,54 @@ CREATE OR REPLACE PROCEDURE prc_gerar_contrato_e_parcelas(
     p_data_inicio DATE,
     p_duracao_meses INT,
     p_valor_mensal NUMERIC(10, 2),
-    p_data_vencimento_dia INT
+    p_dia_vencimento INT
 ) AS $$
 DECLARE
     v_id_contrato INT;
     v_contador INT := 1;
-    v_data_vencimento DATE;
+    v_data_venc DATE;
+    v_data_fim DATE;
 BEGIN
+    v_data_fim := (p_data_inicio + (p_duracao_meses || ' months')::INTERVAL)::DATE;
 
-    INSERT INTO Contrato (
-        fk_inquilino, fk_imovel, data_inicio, status
+    INSERT INTO contrato(
+        fk_inquilino,
+        fk_imovel,
+        clausulas,
+        status,
+        data_inicio,
+        data_fim
     )
     VALUES (
-        p_fk_inquilino, p_fk_imovel, p_data_inicio, 'ativo'
+        p_fk_inquilino,
+        p_fk_imovel,
+        NULL,               
+        'ativo',            
+        p_data_inicio,
+        v_data_fim
     )
     RETURNING id_contrato INTO v_id_contrato;
 
     WHILE v_contador <= p_duracao_meses LOOP
         
-        v_data_vencimento := (p_data_inicio + (v_contador || ' months')::INTERVAL);
-        v_data_vencimento := DATE_TRUNC('month', v_data_vencimento) + (p_data_vencimento_dia - 1 || ' days')::INTERVAL;
-        
-        INSERT INTO Parcela (
-            fk_contrato, numero_parcela, valor, data_vencimento, status
+        v_data_venc :=
+            DATE_TRUNC('month', p_data_inicio + ((v_contador - 1) || ' months')::INTERVAL)
+            + ((p_dia_vencimento - 1) || ' days')::INTERVAL;
+
+        INSERT INTO parcela(
+            fk_contrato,
+            numero_parcela,
+            valor,
+            data_vencimento,
+            data_pagamento,
+            status
         )
         VALUES (
-            v_id_contrato, 
-            v_contador, 
+            v_id_contrato,
+            v_contador,
             p_valor_mensal,
-            v_data_vencimento, 
+            v_data_venc,
+            NULL,          
             'pendente'
         );
 
@@ -43,9 +62,7 @@ BEGIN
 
 EXCEPTION
     WHEN OTHERS THEN
-        RAISE NOTICE 'ERRO: Falha ao gerar contrato e parcelas. Detalhe: %', SQLERRM;
-        ROLLBACK;
-        RAISE EXCEPTION 'PRC_ERROR: Processo de geração de contrato abortado.';
-
+        RAISE NOTICE 'ERRO: %', SQLERRM;
+        RAISE EXCEPTION 'Falha ao gerar contrato e parcelas.';
 END;
 $$ LANGUAGE plpgsql;
