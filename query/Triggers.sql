157f31e274ba29d79fc01fb1c2921a2c1ffc3803
Kauan Selyson <122841788+KSelyson@users.noreply.github.com>
2025-12-04 21:43:04 -0300

    Criando trigger para tabela manutenção

--- query/Triggers.sql
+++ query/Triggers.sql
@@ -1,6 +1,6 @@
 -- == TRIGGERS ==
 
--- 1. Criação de triggers para automatização de logs
+-- 1. Criação de triggers para automatização de logs na tabela parcela
 CREATE OR REPLACE FUNCTION fn_log_op_parcela()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -25,6 +25,28 @@ EXECUTE FUNCTION fn_log_op_parcela();
 
 -- TERMINAR TRIGGERS DE LOGS
 
+-- 2. Criação de trigger para automatização de logs na tabela manutencao
+CREATE OR REPLACE FUNCTION fn_log_op_manutencao()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF(TG_OP = 'INSERT') THEN
+		INSERT INTO log_imobiliaria(data_hora, usuario, tabela_afetada, acao, descricao) VALUES (now(), current_user, 'MANUTENCAO', 'INSERT', 'Manutenção marcada.');
+
+	ELSIF(TG_OP = 'UPDATE') THEN
+		INSERT INTO log_imobiliaria(data_hora, usuario, tabela_afetada, acao, descricao) VALUES (now(), current_user, 'MANUTENCAO', 'UPDATE', 'Manutenção alterada.');
+
+	ELSIF  (TG_OP = 'DELETE')	THEN
+		INSERT INTO log_imobiliaria(data_hora, usuario, tabela_afetada, acao, descricao) VALUES (now(), current_user, 'MANUTENCAO', 'DELETE', 'Manutenção cancelada.');
+		
+	END IF;
+RETURN NULL;		
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_log_op_manutencao
+AFTER INSERT OR UPDATE OR DELETE on parcela
+FOR EACH ROW
+EXECUTE FUNCTION fn_log_op_parcela();
 
 -- 2. REVER ESSA FUNÇÃO/TRIGGER (Obejtivo é fazer uma validação no inquilino antes de realizar um contrato)
 CREATE OR REPLACE FUNCTION fn_validar_novo_contrato()
